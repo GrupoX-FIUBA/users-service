a4c19929fd2445a04db0e5bb274501c559cb5894
ezezbogar <ezezbogar@gmail.com>
2022-05-16 18:00:56 -0300

    datadog config

---
+++ heroku/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+datadog-agent run > /dev/null &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+
+DD_SERVICE="users-service" \
+DD_ENV="prod" \
+DD_LOGS_INJECTION=true \
+ddtrace-run uvicorn app.main:app --host 0.0.0.0 --port $PORT
