de2c4a2068943e35a4e56ca69b0a08600304df06
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-07-05 10:27:33 -0300

    Kubernetes

--- app/api_wallet.py
+++ app/api_wallet.py
@@ -2,6 +2,6 @@ import os
 import requests
 
 def create_wallet (uid : str):
-    result = requests.post("https://spotifiuby-payments-service.herokuapp.com/wallet",
+    result = requests.post(os.environ["PAYMENTS_SERVICE_URL"] + "/wallet",
         params={'user_id': uid},
         headers={'X-API-Key': os.environ['WALLET_API_KEY']})
