0daa943297bf59bed70556d2af4b4bc854d3d827
Nicolas Riedel <riedel@pop-os.localdomain>
2022-07-10 16:44:01 -0300

    add test

--- app/tests/api/test_users.py
+++ app/tests/api/test_users.py
@@ -4,10 +4,16 @@ from sqlalchemy.orm import Session
 from .base import get_valid_api_key   # , get_invalid_api_key
 
 
-def test_read_main(client: TestClient, db: Session):
+def test_read_main_invalid_token(client: TestClient, db: Session):
     response = client.get("/")
     assert response.status_code == 401
-    assert response.json() == {"detail": "Permission denied"}
+
+
+def test_read_main(client: TestClient, db: Session):
+    headers = get_valid_api_key()
+    response = client.get("/", headers=headers)
+    assert response.status_code == 200
+    #assert response.json() == {"detail": "Permission denied"}
 
 
 def test_read_stat(client: TestClient, db: Session):
@@ -15,3 +21,13 @@ def test_read_stat(client: TestClient, db: Session):
     response = client.post("/newPasswordReseted/", headers=headers)
     assert response.status_code == 200
     assert response.json() is None
+
+def test_get_users(client: TestClient, db: Session):
+    headers = get_valid_api_key()
+    response = client.get("/users/", headers=headers)
+    assert response.status_code == 200
+
+def test_cant_users(client: TestClient, db: Session):
+    headers = get_valid_api_key()
+    response = client.get("/users/", headers=headers)
+    assert response.status_code == 200
